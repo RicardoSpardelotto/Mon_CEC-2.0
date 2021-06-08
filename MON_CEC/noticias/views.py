d041f1b4575657f1c5b26602a22174553e0d3079
RicardoSpardelotto <63017205+RicardoSpardelotto@users.noreply.github.com>
2021-06-07 23:22:18 -0300

    Front-004: Update index and make images appear.

--- MON_CEC/noticias/views.py
+++ MON_CEC/noticias/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Noticia
 
 def home(request):
     return render (request, 'index.html')
 
 def noticias(request):
-    return HttpResponse('Area de Noticias')
+    noticias = Noticia.objects.all()
+    return render(request, 'index.html', {'noticias':noticias})
 
