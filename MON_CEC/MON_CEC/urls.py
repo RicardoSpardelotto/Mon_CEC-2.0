2a596499dce3724ed9a8168bd9524917cae9b43d
Ricardo Spardelotto <ricardo.spardelotto@unoesc.edu.br>
2021-06-20 00:13:35 -0300

    Front - 004; Create Apps user, about and home

--- MON_CEC/MON_CEC/urls.py
+++ MON_CEC/MON_CEC/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import urls
 from django.contrib import admin
-from django.urls import path
-from noticias.views import home, noticias
+from django.urls import path, include   
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home),
-    path('noticias', noticias),
+    path('', include('home.urls')),
+    path('', include('noticias.urls')),
+    path('', include('usuario.urls')),
+    path('', include('sobre.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
  
--- MON_CEC/MON_CEC/views.py
+++ MON_CEC/MON_CEC/views.py
@@ -3,5 +3,3 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('Não há nada aqui por enquanto')
 
-def noticias(request):
-    return HttpResponse('Area de Noticias')
